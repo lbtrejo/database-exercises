21f698ec2d6841b5d991dfae61a1652ee2e85eea
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-27 12:19:24 -0500

    feat: deletion update complete

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -5,3 +5,15 @@ SELECT 'Disco Albums' AS '';
 select name, artist from albums where genre = 'Disco';
 SELECT 'Whitney Houston Albums' AS '';
 select name, artist from albums where artist = 'Whitney Houston / Various artists';
+
+DELETE from albums where release_date > 1991;
+DELETE from albums where genre = 'Disco';
+DELETE from albums where artist = 'Whitney Houston / Various artists';
+
+SELECT '==================== POST DELETION ====================' as '';
+SELECT 'Albums after 1991' AS '';
+select name, artist from albums where release_date > 1991;
+SELECT 'Disco Albums' AS '';
+select name, artist from albums where genre = 'Disco';
+SELECT 'Whitney Houston Albums' AS '';
+select name, artist from albums where artist = 'Whitney Houston / Various artists';
