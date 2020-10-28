fa40bf9f6ca0d9053a3abca5d7c3cdc8fdd09434
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-28 11:17:41 -0500

    feat: where_exercise complete

---
+++ where_exercises.sql
@@ -0,0 +1,16 @@
+USE employees;
+
+# Find all employees with first names 'Irena', 'Vidya', or 'Maya'
+SELECT * from employees.employees where first_name IN ('Irena', 'Vidya', 'Maya');
+
+# Find all employees whose last name starts with 'E' — 7,330 rows.
+SELECT * from employees.employees where last_name LIKE 'E%';
+
+# Find all employees hired in the 90s — 135,214 rows.
+SELECT * from employees.employees where hire_date BETWEEN '1990-01-01' AND '1999-12-31';
+
+# Find all employees born on Christmas — 842 rows.
+SELECT * from employees.employees where birth_date LIKE '%12-25';
+
+# Find all employees with a 'q' in their last name — 1,873 rows.
+SELECT * from employees.employees where last_name LIKE '%q%';
