551c4db0f872f8c559495f5fdd9cea4df79ccc72
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-29 17:48:39 -0500

    feat: exercise 5 complete

--- join_exercises.sql
+++ join_exercises.sql
@@ -29,3 +29,13 @@ FROM employees e
          JOIN departments d on de.dept_no = d.dept_no
          JOIN dept_manager dm on d.dept_no = dm.dept_no
 WHERE d.dept_name = 'Customer Service' AND t.to_date = '9999-01-01' AND dm.to_date = '9999-01-01' AND de.to_date = '9999-01-01' GROUP BY t.title;
+
+# Find the current salary of all current managers.
+SELECT d.dept_name as 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Name', s.salary AS 'Salary'
+FROM employees e
+         JOIN titles t on e.emp_no = t.emp_no
+         JOIN dept_emp de on e.emp_no = de.emp_no
+         JOIN departments d on de.dept_no = d.dept_no
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+         JOIN salaries s on e.emp_no = s.emp_no
+WHERE t.title = 'Manager' AND t.to_date = '9999-01-01' AND dm.to_date = '9999-01-01' AND de.to_date = '9999-01-01' AND s.to_date = '9999-01-01' ORDER BY d.dept_name ASC;
