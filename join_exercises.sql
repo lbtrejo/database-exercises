76ba6a8f95af3feab5bcdcfd78a772f32c971d2d
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-29 17:08:10 -0500

    feat: problem 4 solved

--- join_exercises.sql
+++ join_exercises.sql
@@ -20,3 +20,12 @@ FROM employees AS e
          JOIN departments as d
               ON d.dept_no = de.dept_no
 WHERE dm.to_date = '9999-01-01' AND e.gender = 'F' ORDER BY d.dept_name ASC;
+
+# Find the current titles of employees currently working in the Customer Service department.
+SELECT t.title AS 'Title', d.dept_name as 'Department', COUNT(*)
+FROM employees e
+         JOIN titles t on e.emp_no = t.emp_no
+         JOIN dept_emp de on e.emp_no = de.emp_no
+         JOIN departments d on de.dept_no = d.dept_no
+         JOIN dept_manager dm on d.dept_no = dm.dept_no
+WHERE d.dept_name = 'Customer Service' AND t.to_date = '9999-01-01' AND dm.to_date = '9999-01-01' AND de.to_date = '9999-01-01' GROUP BY t.title;
