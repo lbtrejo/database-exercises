1a66e80177728ebb94803eae8af3d5d521498aba
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-29 14:21:15 -0500

    feat: join exercise 2 complete

---
+++ join_exercises.sql
@@ -0,0 +1,11 @@
+# Using the example in the Associative Table Joins section as a guide, write a query that shows each department along with the name of the current manager for that department.
+
+SELECT d.dept_name as 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Department Manager'
+FROM employees AS e
+         JOIN dept_emp as de
+              ON e.emp_no = de.emp_no
+         JOIN dept_manager as dm
+              ON e.emp_no = dm.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE dm.to_date = '9999-01-01' ORDER BY d.dept_name ASC;
