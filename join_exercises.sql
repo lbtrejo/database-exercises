e0bd7805a0b26182a81f3b74e90cd459a2d3dd39
Levi Trejo <levi.b.trejo@gmail.com>
2020-10-29 14:22:36 -0500

    feat: problem 3 solved

--- join_exercises.sql
+++ join_exercises.sql
@@ -9,3 +9,14 @@ FROM employees AS e
          JOIN departments as d
               ON d.dept_no = de.dept_no
 WHERE dm.to_date = '9999-01-01' ORDER BY d.dept_name ASC;
+
+# Find the name of all departments currently managed by women.
+SELECT d.dept_name as 'Department Name', CONCAT(e.first_name, ' ', e.last_name) AS 'Manager Name'
+FROM employees AS e
+         JOIN dept_emp as de
+              ON e.emp_no = de.emp_no
+         JOIN dept_manager as dm
+              ON e.emp_no = dm.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F' ORDER BY d.dept_name ASC;
